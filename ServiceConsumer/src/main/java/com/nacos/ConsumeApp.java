1462b4b24cbc129ad74643d64d2c35f71746a0d3
qinyue530 <530495973@qq.com>
2020-04-10 17:22:32 +0800

    消费者 调用生产者方法

--- ServiceConsumer/src/main/java/com/nacos/ConsumeApp.java
+++ ServiceConsumer/src/main/java/com/nacos/ConsumeApp.java
@@ -3,10 +3,18 @@ package com.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumeApp {
     public static void main(String[] args) {
         SpringApplication.run(ConsumeApp.class);
+    }
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
--- ServiceConsumer/src/main/java/com/nacos/consume/consumeService.java
+++ ServiceConsumer/src/main/java/com/nacos/consume/consumeService.java
@@ -1,25 +1,30 @@
 package com.nacos.consume;
 
+import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class consumeService {
 
     @Autowired
-    DiscoveryClient discoveryClient;
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping("/getService")
     public Object getService(){
-        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("ConsumeService");
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("ProduceService");
         ServiceInstance serviceInstance = serviceInstanceList.get(0);
-
-        return serviceInstance;
+        URI url = serviceInstance.getUri();
+        String result = restTemplate.getForObject(url+"/setService",String.class);
+        return "调用生产者服务 - 返回值" + result;
     }
 
 }
