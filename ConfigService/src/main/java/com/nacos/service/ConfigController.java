db8b7bb9fc05d9a249814226dfc707a6d95fd2e1
qinyue530 <530495973@qq.com>
2020-04-12 00:32:53 +0800

    利用@RefreshScop动态获取nacos的配置信息，注意：@RequestMapping的方法 需为public 否则获取不到值

--- ConfigService/src/main/java/com/nacos/service/ConfigController.java
+++ ConfigService/src/main/java/com/nacos/service/ConfigController.java
@@ -3,12 +3,14 @@ package com.nacos.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
+@RefreshScope
 public class ConfigController {
     public static void main(String[] args) {
         SpringApplication.run(ConfigController.class);
@@ -17,7 +19,7 @@ public class ConfigController {
     private String name;
 
     @RequestMapping("/getName")
-    private String getName(){
+    public String getName(){
         return name;
     }
 }
