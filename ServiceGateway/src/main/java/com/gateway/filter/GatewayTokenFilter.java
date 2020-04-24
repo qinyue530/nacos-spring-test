689e7e2bd2619222a32a1ecc6919848ffe70edfd
qinyue530 <530495973@qq.com>
2020-04-24 15:13:37 +0800

    gateway 添加过滤器

---
+++ ServiceGateway/src/main/java/com/gateway/filter/GatewayTokenFilter.java
@@ -0,0 +1,32 @@
+package com.gateway.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class GatewayTokenFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String str = exchange.getRequest().getQueryParams().getFirst("token");
+        /*str = "1" ;*/
+        if(StringUtils.isEmpty(str)){
+            System.out.println("没有token  ==================================");
+            //自定义返回页面 显示信息
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+            String msg = "token is null";
+            DataBuffer buffer = response.bufferFactory().wrap(msg.getBytes());
+            return response.writeWith(Mono.just(buffer));
+        }
+        return chain.filter(exchange);
+    }
+}
