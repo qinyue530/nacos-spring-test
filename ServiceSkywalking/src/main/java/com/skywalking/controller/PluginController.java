10001f26da281dd59b57dc6efb8b215ddf2076d0
qinyue <530495973@qq.com>
2020-06-03 15:47:00 +0800

    skywalking添加忽略节点,  agent添加插件包apm-trace-ignore-plugin.config  启动参数添加 -Dskywalking.trace.ignore_path=/getTrsceIdAAAAAA   忽略的节点

--- ServiceSkywalking/src/main/java/com/skywalking/controller/PluginController.java
+++ ServiceSkywalking/src/main/java/com/skywalking/controller/PluginController.java
@@ -9,8 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class PluginController {
     @GetMapping("/getTrsceId")
     public String getTrsceId(){
+        //使当前链路报错，并且提示报错信息（skywalking可视化界面中查看）
         ActiveSpan.error(new RuntimeException("test-error"));
+        //打印info信息（skywalking可视化界面中查看）
         ActiveSpan.info("test - info");
+        //打印debug信息（不skywalking可视化界面中查看）
+        ActiveSpan.debug("test - debug");
+        return TraceContext.traceId();
+
+    }
+    @GetMapping("/getTrsceIdAAAAAA")
+    public String getTrsceIdAAAAAA(){
+        //使当前链路报错，并且提示报错信息（skywalking可视化界面中查看）
+        ActiveSpan.error(new RuntimeException("test-error"));
+        //打印info信息（skywalking可视化界面中查看）
+        ActiveSpan.info("test - info");
+        //打印debug信息（不skywalking可视化界面中查看）
         ActiveSpan.debug("test - debug");
         return TraceContext.traceId();
 
