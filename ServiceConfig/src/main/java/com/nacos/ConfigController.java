e91cb7433de8b67bc66f3196fbf322c6a5944f83
qinyue <530495973@qq.com>
2020-07-13 17:19:52 +0800

    添加sptingcloud配置nacos元数据, 添加Java获取服务元数据方法

--- ServiceConfig/src/main/java/com/nacos/ConfigController.java
+++ ServiceConfig/src/main/java/com/nacos/ConfigController.java
@@ -1,16 +1,25 @@
 package com.nacos;
 
 
+import com.alibaba.cloud.nacos.discovery.NacosDiscoveryClient;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @SpringBootApplication
 @RefreshScope
@@ -23,9 +32,17 @@ public class ConfigController implements ApplicationContextAware {
     @Value("${common.name}")
     private String name;
 
-    @RequestMapping("/getName")
-    public String getName(){
-        return name;
+    @RequestMapping("/test/getName")
+    public String getName() throws NacosException {
+        NamingService naming = NamingFactory.createNamingService("127.0.0.1:8848");
+//        System.out.println(naming.toString());
+        List<Instance> instance =naming.getAllInstances("ConfigService");
+        for(Instance i : instance){
+            System.out.println(i.getInstanceId()+" "+i.getServiceName()+" "+i.getIp()+":"+i.getPort()+"实例元数据"+i.getMetadata());
+            System.out.println(i.getMetadata().get("test")+"=============");
+        }
+
+        return name + "================8083=================";
     }
 
     @RequestMapping("/geta")
